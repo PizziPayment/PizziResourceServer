1b1791bf39e47943440c1347ae65ef553fca1241
agarof <alban.garofoli@epitech.eu>
2021-10-29 13:08:34 -0400

    Setup test framework

--- app/index.ts
+++ app/index.ts
@@ -1,31 +1,6 @@
-import * as express from 'express'
-import * as bodyParser from 'body-parser'
-
-import { config } from './common/config'
-import UserRouter from './user/routes.config'
+import Config from './common/config/env.config'
+import { App } from './api'
 import { Orm } from './common/services/orm/orm.service'
-import ShopRouter from './shop/routes.config'
-
-const app = express()
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-
-//Cors
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Credentials', 'true')
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE')
-    res.header('Access-Control-Expose-Headers', 'Content-Length')
-    res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range')
-    if (req.method === 'OPTIONS') {
-        return res.sendStatus(200)
-    } else {
-        return next()
-    }
-})
-
-UserRouter(app)
-ShopRouter(app)
 
 Orm.authenticate()
     .then(() => console.log('Orm synchronised'))
@@ -33,6 +8,6 @@ Orm.authenticate()
         throw new Error("Can't connect to database")
     })
 
-app.listen(config.port, () => {
-    console.log(`API is listening on ${config.port}`)
+App.listen(Config.apiPort, () => {
+    console.log(`API is listening on ${Config.apiPort}`)
 })
