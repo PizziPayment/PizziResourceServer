a87a77a368559cb50617e5e209910b9c967c4328
Krapaince <mathieu.pointecouteau@epitech.eu>
2021-11-30 23:19:30 +0100

    Re-add custom setup.

--- .github/setup/jest.sh
+++ .github/setup/jest.sh
@@ -6,6 +6,6 @@ echo "https://pointecouteau.fr:40403/" >> .yarnrc
 touch .npmrc
 echo "https://pointecouteau.fr:40403/" >> .npmrc
 echo "@pizzi-db:registry=https://pointecouteau.fr:40403/" >> .npmrc
-echo "//pointecouteau.fr:40403/:_authToken=\"${VERDACIO_TOKEN}\"" >> .npmrc
+echo "//pointecouteau.fr:40403/:_authToken=\"${NODE_AUTH_TOKEN}\"" >> .npmrc
 echo "//pointecouteau.fr:40403/:always-auth=true" >> .npmrc
 echo "always-auth=true" >> .npmrc
