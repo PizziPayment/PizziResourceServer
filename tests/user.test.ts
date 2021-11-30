63a7d73c163debf68f0e3adca7d7b3c51403e802
agarof <alban.garofoli@epitech.eu>
2021-11-30 02:55:18 -0500

    Add tests for /users PATCH method

--- tests/user.test.ts
+++ tests/user.test.ts
@@ -1,27 +1,43 @@
 import { App } from '../app/api'
 import { config } from '../app/common/config'
 import * as request from 'supertest'
-import { rewriteTables } from 'pizzi-db'
+import { CredentialModel, rewriteTables, UserModel, UsersServices } from 'pizzi-db'
 import { ClientsService } from 'pizzi-db'
 import { CredentialsService } from 'pizzi-db'
 import { TokensService } from 'pizzi-db'
 import { EncryptionService } from 'pizzi-db'
+
+const user = {
+    name: 'toto',
+    surname: 'tutu',
+    email: 'toto@tutu.tata',
+    password: 'gY@3Cwl4FmLlQ@HycAf',
+    place: {
+        address: '13 rue de la ville',
+        city: 'Ville',
+        zipcode: 25619,
+    },
+}
 
 const client = { client_id: 'toto', client_secret: 'tutu' }
 const client_header = {
     Authorization: 'Basic ' + Buffer.from(`${client.client_id}:${client.client_secret}`).toString('base64'),
 }
 
+async function get_user_credentials(email: string, password: string): Promise<CredentialModel> {
+    return (await CredentialsService.getCredentialFromMailAndPassword(email, EncryptionService.encrypt(password)))._unsafeUnwrap()
+}
+
+async function get_user(email: string, password: string): Promise<UserModel> {
+    const credentials = await get_user_credentials(email, password)
+
+    return (await UsersServices.getUserFromId(credentials.user_id))._unsafeUnwrap()
+}
+
 async function get_user_token(email: string, password: string): Promise<string> {
-    let client_handle = (
-        await ClientsService.getClientFromIdAndSecret(client.client_id, client.client_secret)
-    )._unsafeUnwrap()
-    let credentials = (
-        await CredentialsService.getCredentialFromMailAndPassword(email, EncryptionService.encrypt(password))
-    )._unsafeUnwrap()
-    let token = (
-        await TokensService.generateTokenBetweenClientAndCredential(client_handle, credentials)
-    )._unsafeUnwrap()
+    let client_handle = (await ClientsService.getClientFromIdAndSecret(client.client_id, client.client_secret))._unsafeUnwrap()
+    let credentials = await get_user_credentials(email, password)
+    let token = (await TokensService.generateTokenBetweenClientAndCredential(client_handle, credentials))._unsafeUnwrap()
 
     return token.access_token
 }
@@ -32,7 +48,7 @@ function random_string(length: number): string {
     while (ret.length < length) {
         ret += Math.random()
             .toString(16)
-            .substr(0, length - ret.length)
+            .substring(0, length - ret.length)
     }
 
     return ret
@@ -81,17 +97,7 @@ describe('User endpoint', () => {
             const res = await request(App)
                 .post(endpoint)
                 .set(client_header)
-                .send({
-                    name: 'toto',
-                    surname: 'tutu',
-                    email: 'toto@tutu.tata',
-                    password: 'gY@3Cwl4FmLlQ@HycAf',
-                    place: {
-                        address: '13 rue de la ville',
-                        city: 'Ville',
-                        zipcode: '25619',
-                    },
-                })
+                .send(user)
 
             expect(res.statusCode).toEqual(201)
         })
@@ -100,24 +106,14 @@ describe('User endpoint', () => {
             const first_res = await request(App)
                 .post(endpoint)
                 .set(client_header)
-                .send({
-                    name: 'toto',
-                    surname: 'tutu',
-                    email: 'toto@tutu.tata',
-                    password: 'gY@3Cwl4FmLlQ@HycAf',
-                    place: {
-                        address: '13 rue de la ville',
-                        city: 'Ville',
-                        zipcode: '25619',
-                    },
-                })
+                .send(user)
             const second_res = await request(App)
                 .post(endpoint)
                 .set(client_header)
                 .send({
                     name: 'titi',
                     surname: 'toto',
-                    email: 'toto@tutu.tata',
+                    email: user.email,
                     password: 'gY@3Cwl4FmLlQ@HycAf',
                     place: {
                         address: 'Somewhere',
@@ -136,15 +132,8 @@ describe('User endpoint', () => {
                     .post(endpoint)
                     .set(client_header)
                     .send({
-                        name: 'toto',
-                        surname: 'tutu',
-                        email: 'toto@tutu.tata',
+                        ...user,
                         password: '@bcd3',
-                        place: {
-                            address: '13 rue de la ville',
-                            city: 'Ville',
-                            zipcode: '25619',
-                        },
                     })
 
                 expect(res.statusCode).toEqual(400)
@@ -155,15 +144,8 @@ describe('User endpoint', () => {
                     .post(endpoint)
                     .set(client_header)
                     .send({
-                        name: 'toto',
-                        surname: 'tutu',
-                        email: 'toto@tutu.tata',
+                        ...user,
                         password: 'Abcd3fgh1jklmnOp',
-                        place: {
-                            address: '13 rue de la ville',
-                            city: 'Ville',
-                            zipcode: '25619',
-                        },
                     })
 
                 expect(res.statusCode).toEqual(400)
@@ -174,15 +156,8 @@ describe('User endpoint', () => {
                     .post(endpoint)
                     .set(client_header)
                     .send({
-                        name: 'toto',
-                        surname: 'tutu',
-                        email: 'toto@tutu.tata',
+                        ...user,
                         password: '@bcdEfghIjklmnOp',
-                        place: {
-                            address: '13 rue de la ville',
-                            city: 'Ville',
-                            zipcode: '25619',
-                        },
                     })
 
                 expect(res.statusCode).toEqual(400)
@@ -193,15 +168,8 @@ describe('User endpoint', () => {
                     .post(endpoint)
                     .set(client_header)
                     .send({
-                        name: 'toto',
-                        surname: 'tutu',
-                        email: 'toto@tutu.tata',
+                        ...user,
                         password: '@bcd3fgh1jklmnop',
-                        place: {
-                            address: '13 rue de la ville',
-                            city: 'Ville',
-                            zipcode: '25619',
-                        },
                     })
 
                 expect(res.statusCode).toEqual(400)
@@ -212,15 +180,8 @@ describe('User endpoint', () => {
                     .post(endpoint)
                     .set(client_header)
                     .send({
-                        name: 'toto',
-                        surname: 'tutu',
-                        email: 'toto@tutu.tata',
+                        ...user,
                         password: '@BCD3FGH1JKLMNOP',
-                        place: {
-                            address: '13 rue de la ville',
-                            city: 'Ville',
-                            zipcode: '25619',
-                        },
                     })
 
                 expect(res.statusCode).toEqual(400)
@@ -230,110 +191,170 @@ describe('User endpoint', () => {
 
     describe('DELETE request', () => {
         it('should allow the deletion of a user using a valid password and token', async () => {
-            const email = 'toto@tutu.tata'
-            const password = 'gY@3Cwl4FmLlQ@HycAf'
             const create_res = await request(App)
                 .post(endpoint)
                 .set(client_header)
-                .send({
-                    name: 'toto',
-                    surname: 'tutu',
-                    email,
-                    password,
-                    place: {
-                        address: '13 rue de la ville',
-                        city: 'Ville',
-                        zipcode: '25619',
-                    },
-                })
+                .send(user)
 
             expect(create_res.statusCode).toEqual(201)
 
-            const header = create_bearer_header(await get_user_token(email, password))
-
-            const res = await request(App).delete(endpoint).set(header).send({ password })
+            const header = create_bearer_header(await get_user_token(user.email, user.password))
+            const res = await request(App).delete(endpoint).set(header).send({ password: user.password })
 
             expect(res.statusCode).toEqual(204)
         })
 
         it('should not allow the deletion of a user using an invalid token', async () => {
-            const email = 'toto@tutu.tata'
-            const password = 'gY@3Cwl4FmLlQ@HycAf'
             const create_res = await request(App)
                 .post(endpoint)
                 .set(client_header)
-                .send({
-                    name: 'toto',
-                    surname: 'tutu',
-                    email,
-                    password,
-                    place: {
-                        address: '13 rue de la ville',
-                        city: 'Ville',
-                        zipcode: '25619',
-                    },
-                })
+                .send(user)
 
             expect(create_res.statusCode).toEqual(201)
 
-            const token = await get_user_token(email, password)
+            const token = await get_user_token(user.email, user.password)
             const header = create_bearer_header(create_random_token(token))
-            const res = await request(App).delete(endpoint).set(header).send({ password })
+            const res = await request(App).delete(endpoint).set(header).send({ password: user.password })
 
             expect(res.statusCode).toEqual(401)
         })
 
         it('should not allow the deletion of a user using an invalid password', async () => {
-            const email = 'toto@tutu.tata'
-            const password = 'gY@3Cwl4FmLlQ@HycAf'
             const invalid_password = 'non3Cwl4FmLlQ@HycAf'
             const create_res = await request(App)
                 .post(endpoint)
                 .set(client_header)
-                .send({
-                    name: 'toto',
-                    surname: 'tutu',
-                    email,
-                    password,
-                    place: {
-                        address: '13 rue de la ville',
-                        city: 'Ville',
-                        zipcode: '25619',
-                    },
-                })
+                .send(user)
 
             expect(create_res.statusCode).toEqual(201)
 
-            const header = create_bearer_header(await get_user_token(email, password))
+            const header = create_bearer_header(await get_user_token(user.email, user.password))
             const res = await request(App).delete(endpoint).set(header).send({ password: invalid_password })
 
             expect(res.statusCode).toEqual(403)
         })
 
         it('should not allow the deletion of a user without a password', async () => {
-            const email = 'toto@tutu.tata'
-            const password = 'gY@3Cwl4FmLlQ@HycAf'
             const create_res = await request(App)
                 .post(endpoint)
                 .set(client_header)
-                .send({
-                    name: 'toto',
-                    surname: 'tutu',
-                    email,
-                    password,
-                    place: {
-                        address: '13 rue de la ville',
-                        city: 'Ville',
-                        zipcode: '25619',
-                    },
-                })
+                .send(user)
 
             expect(create_res.statusCode).toEqual(201)
 
-            const header = create_bearer_header(await get_user_token(email, password))
+            const header = create_bearer_header(await get_user_token(user.email, user.password))
             const res = await request(App).delete(endpoint).set(header).send({})
 
             expect(res.statusCode).toEqual(400)
+        })
+    })
+
+    describe('PATCH request', () => {
+        it('should allow the modification of a user\'s name', async () => {
+            const create_res = await request(App)
+                .post(endpoint)
+                .set(client_header)
+                .send(user)
+
+            expect(create_res.statusCode).toEqual(201)
+            let user_data = await get_user(user.email, user.password)
+            expect(user_data.firstname).toEqual(user.name)
+
+            const name = 'woop'
+            const header = create_bearer_header(await get_user_token(user.email, user.password))
+            const patch_res = await request(App)
+                .patch(endpoint)
+                .set(header)
+                .send({ name })
+
+            expect(patch_res.statusCode).toEqual(200)
+            user_data = await get_user(user.email, user.password)
+            expect(user_data.firstname).toEqual(name)
+        })
+
+        it('should allow the modification of a user\'s surname', async () => {
+            const create_res = await request(App)
+                .post(endpoint)
+                .set(client_header)
+                .send(user)
+
+            expect(create_res.statusCode).toEqual(201)
+            let user_data = await get_user(user.email, user.password)
+            expect(user_data.surname).toEqual(user.surname)
+
+            const surname = 'woop'
+            const header = create_bearer_header(await get_user_token(user.email, user.password))
+            const patch_res = await request(App)
+                .patch(endpoint)
+                .set(header)
+                .send({ surname })
+
+            expect(patch_res.statusCode).toEqual(200)
+            user_data = await get_user(user.email, user.password)
+            expect(user_data.surname).toEqual(surname)
+        })
+
+        it('should allow the modification of a user\'s address', async () => {
+            const create_res = await request(App)
+                .post(endpoint)
+                .set(client_header)
+                .send(user)
+
+            expect(create_res.statusCode).toEqual(201)
+            let user_data = await get_user(user.email, user.password)
+            expect(user_data.address).toEqual(`${user.place.address} ${user.place.city}`)
+            expect(user_data.zipcode).toEqual(user.place.zipcode)
+
+            const place = {
+                address: '14 rue de la paville',
+                city: 'Paville',
+                zipcode: 25620,
+            }
+            const header = create_bearer_header(await get_user_token(user.email, user.password))
+            const patch_res = await request(App)
+                .patch(endpoint)
+                .set(header)
+                .send({ place })
+
+            expect(patch_res.statusCode).toEqual(200)
+            user_data = await get_user(user.email, user.password)
+            expect(user_data.address).toEqual(`${place.address} ${place.city}`)
+            expect(user_data.zipcode).toEqual(place.zipcode)
+        })
+
+        it('should not allow the modification of a user\'s informations with an invalid token', async () => {
+            const create_res = await request(App)
+                .post(endpoint)
+                .set(client_header)
+                .send(user)
+
+            expect(create_res.statusCode).toEqual(201)
+
+            const name = 'woop'
+            const token = await get_user_token(user.email, user.password)
+            const header = create_bearer_header(create_random_token(token))
+            const patch_res = await request(App)
+                .patch(endpoint)
+                .set(header)
+                .send({ name })
+
+            expect(patch_res.statusCode).toEqual(401)
+        })
+
+        it('should not allow the modification of a user\'s informations with an invalid token', async () => {
+            const create_res = await request(App)
+                .post(endpoint)
+                .set(client_header)
+                .send(user)
+
+            expect(create_res.statusCode).toEqual(201)
+
+            const name = 'woop'
+            const patch_res = await request(App)
+                .patch(endpoint)
+                .send({ name })
+
+            expect(patch_res.statusCode).toEqual(400)
         })
     })
 })
