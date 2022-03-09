93d20ea1a50c479ef80c7ef5b1159d32f25a463e
agarof <alban.garofoli@epitech.eu>
2022-03-09 13:11:46 -0500

    Add Shop infos route

--- app/shop/controllers/shop.controller.ts
+++ app/shop/controllers/shop.controller.ts
@@ -1,7 +1,17 @@
 import { Request, Response } from 'express'
 import RegisterRequestModel from '../models/register.request.model'
 import { ApiFailure } from '../../common/models/api.response.model'
-import { CredentialsService, EncryptionService, ShopsServices, TokenModel } from 'pizzi-db'
+import { CredentialsService, EncryptionService, ShopsServices, TokenModel, CredentialModel } from 'pizzi-db'
+import InfosResponseModel from '../models/infos.response'
+
+export async function shopInfo(req: Request, res: Response): Promise<void> {
+  const credentials = res.locals.credential as CredentialModel
+
+  await ShopsServices.getShopFromId(credentials.shop_id).match(
+    (shop) => res.status(200).send(new InfosResponseModel(credentials.email, shop)),
+    () => res.status(500).send(new ApiFailure(req.url, 'Internal error')),
+  )
+}
 
 export async function register(req: Request<unknown, unknown, RegisterRequestModel>, res: Response): Promise<void> {
   await ShopsServices.createShop(req.body.name, req.body.phone, `${req.body.place.address} ${req.body.place.city}`, req.body.place.zipcode)
--- app/shop/routes.config.ts
+++ app/shop/routes.config.ts
@@ -1,5 +1,5 @@
 import { Application } from 'express'
-import { register, deleteAccount } from './controllers/shop.controller'
+import { register, deleteAccount, shopInfo } from './controllers/shop.controller'
 import validBasicAuth from '../common/middlewares/basic_auth.validation.middleware'
 import validRegisterRequest from './middlewares/register.request.validation.middleware'
 import validUniqueEmail from '../common/middlewares/unique_email.validation.middleware'
@@ -10,12 +10,14 @@ import changePassword from '../common/controllers/password.controller'
 import validChangePasswordRequest from '../common/middlewares/password.request.validation.middleware'
 import { validChangeEmailRequest } from '../common/middlewares/email.request.validation.middleware'
 import changeEmail from '../common/controllers/email.controller'
+import validShopTokenAffiliation from '../shop/middlewares/shop_token_affiliation.validation'
 
 export const baseUrl = '/shops'
 export const baseUrlPassword = `${baseUrl}/me/password`
 export const baseUrlEmail = `${baseUrl}/me/email`
 
 export default function ShopRouter(app: Application): void {
+  app.get(`${baseUrl}/`, [validToken, validShopTokenAffiliation, shopInfo])
   app.post(`${baseUrl}/`, [validBasicAuth, validRegisterRequest, validUniqueEmail, register])
   app.delete(`${baseUrl}/`, [validToken, validDeleteRequest, validPassword, deleteAccount])
   app.put(`${baseUrlPassword}/`, [validToken, validChangePasswordRequest, validChangePassword, changePassword])
