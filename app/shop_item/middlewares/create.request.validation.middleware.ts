68aa6598f0010fa1cdf200402aed66b6159862d1
Krapaince <mathieu.pointecouteau@epitech.eu>
2022-03-10 02:10:59 +0100

    Fix shop items's creation validation middleware.

--- app/shop_item/middlewares/create.request.validation.middleware.ts
+++ app/shop_item/middlewares/create.request.validation.middleware.ts
@@ -10,18 +10,23 @@ export async function validShopItemsCreation(
   const errors: Array<string> = []
 
   if (req.body) {
-    if (req.body.items === undefined && Array.isArray(req.body.items)) {
+    if (req.body.items === undefined || Array.isArray(req.body.items) == false) {
       errors.push('invalid items')
+    } else {
+      if (req.body.items.length === 0) {
+        errors.push('not shop item provided in items')
+      }
+
+      for (const [i, item] of req.body.items.entries()) {
+        if (item.name === undefined) {
+          errors.push(`invalid name in item n#${i + 1}`)
+        }
+        if (item.price === undefined) {
+          errors.push(`invalid price in item n#${i + 1}`)
+        }
+      }
     }
 
-    for (const [i, item] of req.body.items.entries()) {
-      if (item.name === undefined) {
-        errors.push(`invalid name in item n#${i + 1}`)
-      }
-      if (item.price === undefined) {
-        errors.push(`invalid price in item n#${i + 1}`)
-      }
-    }
     if (errors.length === 0) {
       return next()
     } else {
--- app/shop_item/routes.config.ts
+++ app/shop_item/routes.config.ts
@@ -3,11 +3,12 @@ import validToken from '../common/middlewares/token.validation.middleware'
 import { baseUrl as shopBaseUrl } from '../shop/routes.config'
 import validShopTokenAffiliation from '../shop/middlewares/shop_token_affiliation.validation'
 import { createShopItems, retrieveShopItems } from './controllers/shop_item.controller'
+import { validShopItemsCreation } from './middlewares/create.request.validation.middleware'
 import { validShopItemsRetrieval } from './middlewares/retrieve_param.validation.middleware'
 
 export const baseUrl = `${shopBaseUrl}/me/items`
 
 export default function ShopItemRouter(app: Application): void {
-  app.post(`${baseUrl}/`, [validToken, validShopTokenAffiliation, createShopItems])
+  app.post(`${baseUrl}/`, [validToken, validShopTokenAffiliation, validShopItemsCreation, createShopItems])
   app.get(`${baseUrl}/`, [validToken, validShopTokenAffiliation, validShopItemsRetrieval, retrieveShopItems])
 }
