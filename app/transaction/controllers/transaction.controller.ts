4a2a3a820d2a3b59c4badd473482b81710acdbbd
Lilian VERLHAC <lilian.verlhac@outlook.com>
2022-05-21 17:50:33 +0100

    Add get transactions by state with state query (untested)

--- app/transaction/controllers/transaction.controller.ts
+++ app/transaction/controllers/transaction.controller.ts
@@ -1,12 +1,22 @@
 import { Request, Response } from 'express'
-import { TransactionsService } from 'pizzi-db'
+import { CredentialModel, TransactionsService } from 'pizzi-db'
 import { TransactionCreationModel } from '../models/create.request.model'
 import { ApiFailure } from '../../common/models/api.response.model'
 import { TransactionResponseModel } from '../models/response.model'
+import { TransactionStateQuery } from '../models/retrieve.request.model'
 
 export async function createTransaction(req: Request<unknown, unknown, TransactionCreationModel>, res: Response): Promise<void> {
   await TransactionsService.createPendingTransaction(req.body.receipt_id, req.body.user_id, req.body.shop_id, req.body.payment_method).match(
     (transaction) => res.status(201).send(new TransactionResponseModel(transaction)),
+    () => res.status(500).send(new ApiFailure(req.url, 'Internal error')),
+  )
+}
+
+export async function getTransactions(req: Request<unknown, unknown, TransactionCreationModel, TransactionStateQuery>, res: Response): Promise<void> {
+  const credentials = res.locals.credential as CredentialModel
+
+  await TransactionsService.getOwnerTransactionsByState('shop', credentials.shop_id, req.query.state).match(
+    (transactions) => res.status(200).send(transactions.map((transaction) => new TransactionResponseModel(transaction))),
     () => res.status(500).send(new ApiFailure(req.url, 'Internal error')),
   )
 }
--- app/transaction/routes.config.ts
+++ app/transaction/routes.config.ts
@@ -1,11 +1,13 @@
 import { Application } from 'express'
-import { createTransaction } from './controllers/transaction.controller'
+import { createTransaction, getTransactions } from './controllers/transaction.controller'
 import validToken from '../common/middlewares/token.validation.middleware'
 import validShopTokenAffiliation from '../common/middlewares/shop_token_affiliation.validation.middleware'
 import validCreateRequest from './middlewares/create.request.validation.middleware'
+import { validTransactionRetrievalQuery } from './middlewares/retrieve_param.validation.middlewares'
 
 export const baseUrl = `/transactions`
 
 export default function TransactionsRouter(app: Application): void {
   app.post(`${baseUrl}/`, [validToken, validShopTokenAffiliation, validCreateRequest, createTransaction])
+  app.get(`${baseUrl}/`, [validToken, validShopTokenAffiliation, validTransactionRetrievalQuery, getTransactions])
 }
