2edbd57b6aa4ac92dd7358a916f1a123d704ff53
Mathieu Pointecouteau <mathieu.pointecouteau@epitech.eu>
2022-11-01 18:38:13 +0100

    Move parseAuthHeader function to services.

--- app/common/middlewares/authorization.middleware.ts
+++ app/common/middlewares/authorization.middleware.ts
@@ -1,8 +1,8 @@
 import { NextFunction, Request, Response } from 'express'
-import { Err, Ok, Result } from 'neverthrow'
 import { ClientsService, ErrorCause, TokensService } from 'pizzi-db'
 import { ApiFailure, ApiResponseWrapper } from '../models/api.response.model'
 import { AuthErrorMsg, AuthKind, BasicAuth, BearerAuth } from '../models/authorization.model'
+import { parseAuthHeader } from '../services/authorization'
 import { createResponseHandler } from '../services/error_handling'
 
 export async function validAccessToken(req: Request, res: Response<ApiResponseWrapper<unknown>>, next: NextFunction): Promise<void> {
@@ -43,47 +43,4 @@ export async function validBasicAuth(req: Request, res: Response<ApiResponseWrap
       res.status(code).send(new ApiFailure(req.url, message))
     },
   )
-}
-
-function parseAuthHeader(authorization: any, expected_auth_methods: Array<AuthKind>): Result<BearerAuth | BasicAuth, { code: number; message: string }> {
-  if (typeof authorization !== 'string') {
-    return new Err({ code: 400, message: AuthErrorMsg.InvalidAuthHeader })
-  }
-
-  const auth_header_parts = authorization.split(' ')
-  if (auth_header_parts.length !== 2) {
-    return new Err({ code: 400, message: AuthErrorMsg.InvalidAuthHeader })
-  }
-
-  const [type, token] = auth_header_parts
-  let auth: BearerAuth | BasicAuth = null
-
-  if (!expected_auth_methods.includes(type as AuthKind)) {
-    return new Err({ code: 400, message: AuthErrorMsg.InvalidOrUnknownAuthMethod })
-  }
-
-  switch (type) {
-    case 'Bearer':
-      auth = {
-        kind: AuthKind.Bearer,
-        access_token: token,
-      }
-      break
-    case 'Basic':
-      const client_parts = Buffer.from(token, 'base64').toString('ascii').split(':')
-      const [client_id, client_secret] = client_parts
-
-      if (client_parts.length !== 2 || client_id === '' || client_secret === '') {
-        return new Err({ code: 400, message: AuthErrorMsg.NoClientCredGiven })
-      }
-
-      auth = {
-        kind: AuthKind.Basic,
-        client_id: client_parts[0],
-        client_secret: client_parts[1],
-      }
-      break
-  }
-
-  return new Ok(auth)
 }
